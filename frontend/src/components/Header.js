4ee4e83624046f8fa008e9e69b6783ac5a4e21be
MohamedAliTaha <mohamadalitaha03@gmail.com>
2023-01-22 09:59:16 -0500

    fixed the header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,5 @@
 import logo from "./logo.png";
+import { Link } from "react-router-dom";
 
 const headerStyles = {
     position: "absolute",
@@ -6,16 +7,21 @@ const headerStyles = {
 
 function Header() {
     return (
-        <div className="header">
-            <div className="container">
-                <div className="row">
-                    <div className="col-xl-3 col-lg-3 col-md-3 col-sm-3 col logo_section">
-                        <div className="full">
-                            <div className="center-desk">
-                                <div className="logo">
-                                    <a href="/">
-                                        <img src={logo} alt="#" />
-                                    </a>
+        <div>
+            <Link to="/">Password Evaluation</Link>
+            <br />
+            <Link to="/about">About us</Link>
+            <div className="header">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-xl-3 col-lg-3 col-md-3 col-sm-3 col logo_section">
+                            <div className="full">
+                                <div className="center-desk">
+                                    <div className="logo">
+                                        <a href="/">
+                                            <img src={logo} alt="#" />
+                                        </a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
